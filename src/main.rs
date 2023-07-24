8540a961a8841e7f797d75a46127780db596a8e1
8Oldr <8Oldr@protonmail.com>
2023-07-24 16:39:44 +0300

    v0.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("Currently takes argument 1 and compresses it badly");
+    println!("{} compressed: {}", args[1], compress(&args[1]));
+}
+
+
+fn compress(data: &String) -> String {
+    String::from(data)
 }
